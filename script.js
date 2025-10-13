08c97cab3a4849b00ed5df1d2530071f69c5960c
Paul Dela Bueno <pjaydlbn38@gmail.com>
2025-10-13 17:56:29 +0800

    backend

---
+++ script.js
@@ -0,0 +1,36 @@
+const dialog = document.getElementById('postDialog');
+const createBtn = document.getElementById('createPostBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+const form = document.getElementById('postForm');
+
+    createBtn.addEventListener('click', () => {
+      dialog.showModal();
+    });
+
+    cancelBtn.addEventListener('click', () => {
+      dialog.close();
+    });
+
+const postsContainer = document.getElementById('posts');
+const samplePost = document.getElementById('samplePost');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = form.postTitle.value.trim();
+  const tags = form.postTags.value.trim();   
+  const description = form.postDescription.value.trim();
+
+  const newPost = samplePost.cloneNode(true);
+
+  const titleElement = newPost.querySelector('#mainPostTitle');
+  titleElement.textContent = title;
+
+  const timeElement = newPost.querySelector('#postAuthordesc p');
+  timeElement.textContent = 'Just now';
+
+  postsContainer.appendChild(newPost);
+
+  dialog.close();
+  form.reset();
+});
